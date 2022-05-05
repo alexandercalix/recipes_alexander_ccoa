581f3a4639e22699df3b4c0f186d51b37a807be4
alexandercalix <59989567+alexandercalix@users.noreply.github.com>
2022-05-04 20:40:28 -0500

    Agregar archivos de proyecto.

---
+++ recipes_alexander_ccoa/Shared/Services/HttpService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace recipes_alexander_ccoa.Shared.Services
+{
+    public class HttpService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        public async Task<T> Run<T>(string url, HttpMethod httpMethod, object data )
+        {
+
+            HttpRequestMessage msg = new HttpRequestMessage(httpMethod, url);
+            msg.Content = JsonContent.Create(msg);
+
+            HttpResponseMessage result = await _httpClient.SendAsync(msg);
+
+           
+
+            int statusCode = (int)result.StatusCode;
+
+            if (statusCode >= 400 && statusCode < 499) throw new Exception("Error en Encontrar el Servidor");
+            if (statusCode >= 500)
+            {
+                throw new Exception("Error en el Servidor");
+            }
+
+            if (result.IsSuccessStatusCode)
+            {
+                T value = await result.Content.ReadFromJsonAsync<T>();
+
+
+                return (value);
+            }
+
+            return default(T);
+
+        }
+
+    }
+}
